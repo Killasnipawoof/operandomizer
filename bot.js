92dad9f5f5a339def04377ae6ef8f3bdf67802a9
5donuts <charliegerman32@gmail.com>
2018-04-29 15:24:16 -0400

    added simple command handler

--- bot.js
+++ bot.js
@@ -1,37 +1,35 @@
 const auth = require('./auth.json')
 const config = require('./config.json')
+
 const fs = require('fs')
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
 bot.on("ready", () => {
-  console.log("I am ready!");
+  console.log("Operandomizer is ready.");
 });
 
 bot.on("message", (message) => {
-  // ignore messages that do not start with the command prefix
-  // also, ignore messages sent by other bots
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  // ignore messages sent by bots
+  if (message.author.bot) return;
+  // ignore messages not starting with the command prefix
+  if (!message.content.startsWith(config.prefix)) return;
 
   // split the message into the command and arguments
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  switch(command) {
-    // change the command prefix
-    case "prefix":
-      // only allow the owner to execute this command
-      if(message.author.id !== config.ownerID) return;
-      // get the new prefix from the command
-      let newPrefix = message.content.split(' ').slice(1, 2)[0];
-      // change the configuration in memory
-      config.prefix = newPrefix;
-      // save the new configuration
-      fs.writeFile('./config.json', JSON.stringify(config), (err) => console.error);
-      break;
-  }
+  console.log("Received command: " + command);
 
+  // simple command handler
+  try {
+    let commandFile = require('./commands/' + command + '.js');
+    commandFile.run(bot, message, args);
+  } catch (err) {
+    console.error(err);
+    message.channel.send("'" + config.prefix + command + "' is not a recognized command.");
+  }
 });
 
 bot.login(auth.token);
