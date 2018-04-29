eb173547e485972377c11436d6678bd0651b1d8a
5donuts <charliegerman32@gmail.com>
2018-04-29 19:33:00 -0400

    added help command

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+  message.channel.send("Operandomizer is a Discord bot that randomly selects an "
+    + "operator from \"Tom Clancy's Rainbow Six: Siege.\"\nTo select a random "
+    + "attacker, say " + client.config.prefix + "atk.\nTo select a random defender, "
+    + "say " + client.config.prefix + "def.\nFor more detailed instructions, visit "
+    + "https://github.com/5donuts/operandomizer");
+}
