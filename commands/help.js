7d298c444642aeb0666e18ef7f8329f66bfb7dff
5donuts <charliegerman32@gmail.com>
2018-06-02 16:50:10 -0400

    fixes #7

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,9 @@
 exports.run = (client, message, args) => {
-  message.channel.send("Operandomizer is a Discord bot that randomly selects an "
+  message.channel.send(message.author.username + ": I sent you a DM with some documentation."
+    + "\nThe prefix for this guild is `" + client.config.prefix + "`");
+  message.author.send("Operandomizer is a Discord bot that randomly selects an "
     + "operator from \"Tom Clancy's Rainbow Six: Siege.\"\nTo select a random "
-    + "attacker, say " + client.config.prefix + "atk.\nTo select a random defender, "
-    + "say " + client.config.prefix + "def.\nFor more detailed instructions, visit "
+    + "attacker, say `" + client.config.prefix + "atk`.\nTo select a random defender, "
+    + "say `" + client.config.prefix + "def`.\nFor more detailed instructions, visit "
     + "https://github.com/5donuts/operandomizer#operandomizer");
 }
