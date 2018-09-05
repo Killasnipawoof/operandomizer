7e7a623bbb17f486d53afae3ec667c55de70858e
5donuts <charliegerman32@gmail.com>
2018-09-05 17:28:49 -0400

    started to implement CTU blacklisting

--- helper.js
+++ helper.js
@@ -1,9 +1,21 @@
 exports.run = (client, message, args, isAtk) => {
-  operators = []
+  operators = [];
+  blacklisted_ctus = [];
+  blacklisted_ops = [];
 
   ctulist = isAtk ? client.attackers : client.defenders;
 
-  // no CTUs specified, load all CTUs
+  isCTU = function(arg) {
+    keys = Object.keys(ctulist);
+    for (i = 0; i < keys.length; i++) {
+      if (keys[i] === arg) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  // no arguments given, load all CTUs
   if (args.length === 0) {
     for (ctu in ctulist) {
       ctuOps = ctulist[ctu];
@@ -13,31 +25,68 @@ exports.run = (client, message, args, isAtk) => {
     }
   }
 
-  // load specified CTUs
+  // process the arguments
   else {
     for (i = 0; i < args.length; i++) {
-      ctu = args[i].toLowerCase();
+      arg = args[i].toLowerCase();
 
-      // handle the 'default' CTU
-      if (ctu === "default") {
-        let defaultCTUs = [ "other", "sas", "fbi", "gign", "spetsnaz", "gsg9" ];
-        for (j = 0; j < defaultCTUs.length; j++) {
-          ctu = defaultCTUs[j];
-          ctuOps = ctulist[ctu];
-          for (k = 0; k < ctuOps.length; k++) {
-            operators.push(ctuOps[k]);
-          }
+      // add blacklist args to blacklist
+      if (arg.startsWith("-")) { // TODO this doesn't see the last element for some reason
+        stripped = arg.substring(1); // strip leading '-'
+        if (isCTU(stripped)) {
+          blacklisted_ctus.push(stripped);
+        }
+        else {
+          // capitalize the first letter of stripped
+          blacklisted_ops.push(stripped.charAt(0).toUpperCase() + stripped.slice(1));
         }
       }
-      // handle other CTUs
+
+      // add ctus to the list
       else {
-        ctuOps = ctulist[ctu];
-        for (j = 0; j < ctuOps.length; j++) {
-          operators.push(ctuOps[j]);
+        ctu = arg;
+        // handle the 'default' CTU
+        if (ctu === "default") {
+          let defaultCTUs = [ "other", "sas", "fbi", "gign", "spetsnaz", "gsg9" ];
+          for (j = 0; j < defaultCTUs.length; j++) {
+            ctu = defaultCTUs[j];
+            ctuOps = ctulist[ctu];
+            for (k = 0; k < ctuOps.length; k++) {
+              operators.push(ctuOps[k]);
+            }
+          }
+        }
+
+        // handle other CTUs
+        else {
+          ctuOps = ctulist[ctu];
+          for (j = 0; j < ctuOps.length; j++) {
+            operators.push(ctuOps[j]);
+          }
         }
       }
     }
   }
+
+  // get list of blacklisted ops from blacklisted CTUs
+  for (i = 0; i < blacklisted_ctus.length; i++) {
+    ctuOps = ctulist[blacklisted_ctus[i]];
+    for (j = 0; j < ctuOps.length; j++) {
+      blacklisted_ops.push(ctuOps[j]);
+    }
+  }
+
+  console.log(blacklisted_ctus);
+  console.log(blacklisted_ops);
+  console.log(operators);
+  // remove blacklisted operators from the operator list
+  for (i = 0; i < blacklisted_ops.length; i++) {
+    index = operators.indexOf(blacklisted_ops[i]);
+    if (index > -1) {
+      operators.splice(index, 1); // remove the operator
+    }
+  }
+  console.log(operators);
 
   // randomly select an operator from the list
   selection = Math.floor(Math.random() * operators.length);
